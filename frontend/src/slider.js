a51ae02c647a91a412ceebd7e428d42125cf71b2
Marcel Link <marcel.link@t-online.de>
2018-12-07 16:52:40 +0100

    # touch slider

--- frontend/src/slider.js
+++ frontend/src/slider.js
@@ -1,6 +1,6 @@
 const $ = require('jquery');
 require('jquery-ui/ui/widgets/slider');
-require('jquery-ui-touch-punch');
+require('jquery.ui.touch/jquery.ui.touch.js');
 require('jquery-ui/themes/base/theme.css');
 require('jquery-ui/themes/base/slider.css');
 const pixi = require('./pixi.js');
@@ -17,6 +17,7 @@ function init() {
     }).on( "slide", ( event, ui ) => {
         pixi.v.rotation = (ui.value-180)/360*2*Math.PI;
     });
+    $("#slider-rotate .ui-slider-handle").addTouch();
 }
 
 exports.init = init;
