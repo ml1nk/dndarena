148261365e17c8dbf36a42b2ee1ba5903f78ca4c
Marcel Link <marcel.link@t-online.de>
2018-12-07 19:53:05 +0100

    ^ webpack -  smart build

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: {
+      'postcss-import': {},
+      'postcss-preset-env': {},
+      'cssnano': {
+            discardComments: {
+                removeAll: true
+            }
+      }
+    }
+  }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const prod = process.env.NODE_ENV === "production";
+
+// the path(s) that should be cleaned
+let pathsToClean = [
+  "dist/*.*"
+];
+
+// the clean options to use
+let cleanOptions = {
+  root: __dirname,
+  exclude: ['.gitignore'],
+  verbose: true,
+  dry: false
+}
 
 module.exports = {
   mode: prod ? 'production' : 'development',
@@ -13,23 +29,30 @@ module.exports = {
     publicPath: "dist/"
   },
   module: {
-    rules: [
+    rules: [{
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader',
+          options: {
+            sourceMap: true
+          }
+        }, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+          }
+        }]
+      },
       {
         test: /\.(png|eot|woff|woff2|ttf|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
+        use: [{
+          loader: 'file-loader',
+          options: {}
+        }]
       },
       {
         test: /\.(html)$/,
         use: ['html-loader']
-      },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
       }
     ]
   },
@@ -41,6 +64,32 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'frontend/index.html'
-    })
+    }),
+    new CleanWebpackPlugin(pathsToClean, cleanOptions)
   ]
 };
+
+if (prod) {
+
+  module.exports.module.rules[0].use[0] = {
+    loader: MiniCssExtractPlugin.loader,
+    options: {}
+  }
+
+  module.exports.module.rules[0].use[1].options.importLoaders = 1;
+
+  module.exports.module.rules[0].use.push({
+    loader: 'postcss-loader',
+    options: { sourceMap: true }
+  });
+
+  module.exports.plugins.push(new MiniCssExtractPlugin({
+    filename: "[name].[hash].css",
+    chunkFilename: "[id].[name].[hash].css"
+  }));
+
+  module.exports.plugins.push(new BundleAnalyzerPlugin({
+    openAnalyzer: false,
+    analyzerMode: "static"
+  }));
+}
