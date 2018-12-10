999371c6b12b7a5cfca173f9ccd762007ebe2aec
Marcel Link <marcel.link@t-online.de>
2018-12-10 15:55:18 +0100

    # chat

--- frontend/src/chat.js
+++ frontend/src/chat.js
@@ -1,18 +1,17 @@
 const $ = require('jquery');
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
-const dialog = require('./startup.js');
 const io = require('./io.js');
 
-function init(){
+function init(me){
     $("#button-chat").show();
     $('#chatbox').show();
     $("#txtsendtext").keyup((e)=>{if (e.keyCode === 13) $("#btnchat").click()});
     $('#btnchat').click(()=>{
         let value = $('#txtsendtext').val();
         $("#txtsendtext").val('');
-        io.message.out(dialog.name() + ": " + value);
-        add(dialog.name() + ": " + value, true);
+        io.message.out(me.name + ": " + value);
+        add(me.name + ": " + value, true);
     });
     io.message.in(text=>add(text, false));
 }
