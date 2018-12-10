8bc92c9ca72cff294b84687ddcc7b8779f609fbb
Marcel Link <marcel.link@t-online.de>
2018-12-10 15:42:32 +0100

    # reformat context.js, mouseclick event

--- frontend/src/context.js
+++ frontend/src/context.js
@@ -8,49 +8,62 @@ const pixi = require('./pixi.js');
 const calc = require('../lib/calc.js');
 
 function init(me) {
-    if(!me.gm) return;
+    if (!me.gm) return;
 
     let field;
-    let set = key=>{
-        let obj = io.state.get().field[calc.to(field.x,field.y,field.z)];
-        io.state.set("field."+ calc.to(field.x,field.y,field.z),{ type :key, visible : obj && obj.visible});
+    let set = key => {
+        let obj = io.state.get().field[calc.to(field.x, field.y, field.z)];
+        io.state.set("field." + calc.to(field.x, field.y, field.z), {
+            type: key,
+            visible: obj && obj.visible
+        });
     };
-    let data = config.types.map(vl=>{ return {name:vl[1],callback:()=>set(vl[0])}}).concat(["---------",{name:"Clear",callback : ()=>set(null)}]);
+    let data = config.types.map(vl => {
+        return {
+            name: vl[1],
+            callback: () => set(vl[0])
+        }
+    }).concat(["---------", {
+        name: "Clear",
+        callback: () => set(null)
+    }]);
 
     let time = 0;
-    pixi.v.on("click",e=>{
-        if(time+200<Date.now()) {
+    $(document).click((b) => {
+        if (b.which !== 1) return;
+        let pt = pixi.v.toLocal(new PIXI.Point(b.clientX, b.clientY));
+        if (time + 200 < Date.now()) {
             time = Date.now();
             return;
         }
-        let local = e.data.getLocalPosition(pixi.v);
-        let [x,y,z] = calc.pixel_to_cube(local.x,local.y);
-        let obj = io.state.get().field[calc.to(x,y,z)];
-
-        if(!obj) return;
-
+        let [x, y, z] = calc.pixel_to_cube(pt.x, pt.y);
+        let obj = io.state.get().field[calc.to(x, y, z)];
+        if (!obj) return;
         obj.visible = !obj.visible;
-        io.state.set("field."+calc.to(x,y,z), obj);
+        io.state.set("field." + calc.to(x, y, z), obj);
     });
 
-    pixi.v.on("rightclick",e=>{
+    pixi.v.on("rightclick", e => {
         let local = e.data.getLocalPosition(pixi.v);
-        let [x,y,z] = calc.pixel_to_cube(local.x,local.y);
+        let [x, y, z] = calc.pixel_to_cube(local.x, local.y);
         _field(e.data.global, x, y, z);
     });
 
     $.contextMenu({
-        selector: '#context-menu-field', 
+        selector: '#context-menu-field',
         trigger: 'none',
-        position: opt=>opt.$menu.css({top: field.global.y, left: field.global.x}),
-        build: function($trigger, e) {
+        position: opt => opt.$menu.css({
+            top: field.global.y,
+            left: field.global.x
+        }),
+        build: function ($trigger, e) {
             // this callback is executed every time the menu is to be shown
             // its results are destroyed every time the menu is hidden
             // e is the original contextmenu event, containing e.pageX and e.pageY (amongst other data)
             return {
                 items: {
                     "fold1": {
-                        "name": "Type", 
+                        "name": "Type",
                         "items": data
                     }
                 }
@@ -60,10 +73,10 @@ function init(me) {
 
     function _field(global, x, y, z) {
         field = {
-            global : global,
-            x : x,
-            y : y,
-            z : z
+            global: global,
+            x: x,
+            y: y,
+            z: z
         }
         $('#context-menu-field').contextMenu();
     }
