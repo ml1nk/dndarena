21d5384d7632051be55f8df7cfa82f538c93d26c
Marcel Link <marcel.link@t-online.de>
2019-12-24 21:25:40 +0100

    # service worker path

--- frontend/src/pwa.js
+++ frontend/src/pwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('dist/service-worker.js');
+        navigator.serviceWorker.register('service-worker.js');
     });
 }
 
